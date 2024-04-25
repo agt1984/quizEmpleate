62814f1fb0fbf86b7809d594cbda1156ba831568
Isarok <isaac.lucerito@gmail.com>
2024-04-25 18:25:00 +0200

    fixed Navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           style={{ width: "160px", height: "auto", borderRadius: "15px" }}
         />
         <div className="flex items-center">
-          <ul className="hidden mdl:inline-flex items-center gap-10">
+          <ul className="hidden md:inline-flex items-center gap-10">
             <li className="text-white tracking-wide hover:text-stone-300 text-sm font-bold transition cursor-pointer">
               <Link to="https://empleate-con-talento-front.vercel.app">
                 INICIO
@@ -45,13 +45,13 @@ const Navbar = () => {
           </ul>
           <span
             onClick={() => setShowMenu(!showMenu)}
-            className="text-3xl mdl:hidden w-10 h-10 inline-flex items-center justify-center text-white rounded-full cursor-pointer"
+            className="text-3xl md:hidden w-10 h-10 inline-flex items-center justify-center text-white rounded-full cursor-pointer"
           >
             <CiMenuBurger />
           </span>
         </div>
         {showMenu && (
-          <div className="w-[80%] h-screen mdl:hidden overflow-scroll absolute top-0 left-0 bg-red-800/95 p-4 scrollbar-hide">
+          <div className="w-[80%] h-screen mdl:hidden overflow-hidden absolute top-0 left-0 bg-red-800/95 p-4 scrollbar-hide">
             <div className="flex flex-col gap-8 py-2 relative">
               <div>
                 <img className="w-36 rounded-md" src={Logo} alt="logo" />
